f22b67873530f6602213cf2ebe54a784aa31a061
yukinashi <jdgjxxchy@gmail.com>
2023-06-21 13:03:46 +0800

    upgrade dependency

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 // i18n
 import { createI18n } from 'vue-i18n';
-import messages from '@intlify/vite-plugin-vue-i18n/messages';
+import messages from '@intlify/unplugin-vue-i18n/messages';
 
 // vue router
 import router from '@/router/index';
